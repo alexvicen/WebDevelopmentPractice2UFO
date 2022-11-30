83a717d72f4bac28ad64a00ab7c16b19d37dc320
vicen <avicente@webel.com>
2022-11-30 18:45:05 +0100

    inicio practica 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { RegisterComponent } from './register/register.component';
+import { RecordsComponent } from './records/records.component';
+import { PreferencesComponent } from './preferences/preferences.component';
+import { GameComponent } from './game/game.component';
+import { LoginComponent } from './login/login.component';
+import { IndexComponent } from './index/index.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: 'index', component: IndexComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'game', component: GameComponent },
+  { path: 'preferences', component: PreferencesComponent },
+  { path: 'records', component: RecordsComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { GameComponent } from './game/game.component';
+import { PreferencesComponent } from './preferences/preferences.component';
+import { RecordsComponent } from './records/records.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IndexComponent,
+    LoginComponent,
+    GameComponent,
+    PreferencesComponent,
+    RecordsComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
